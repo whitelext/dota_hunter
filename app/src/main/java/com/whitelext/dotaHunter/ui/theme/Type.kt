5b417429f3064ac98eac086fec570ee49033ce31
Aleksei Chemiakin <whitelext@gmail.com>
2021-10-22 12:33:39 +0300

    detekt fixes

--- app/src/main/java/com/whitelext/dotaHunter/ui/theme/Type.kt
+++ app/src/main/java/com/whitelext/dotaHunter/ui/theme/Type.kt
@@ -6,12 +6,11 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-const val FONT_SIZE_16 = 16
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = FONT_SIZE_16.sp
+        fontSize = 16.sp
     )
     /* Other default text styles to override
     button = TextStyle(
