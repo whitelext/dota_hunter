8897d3f66ef4be9379df2414fc808e5066242742
SirDratuti <belitsckiy@mail.ru>
2021-12-13 00:19:48 +0300

    simple splash screen

--- app/src/main/java/com/whitelext/dotaHunter/view/screens/MainActivity.kt
+++ app/src/main/java/com/whitelext/dotaHunter/view/screens/MainActivity.kt
@@ -3,8 +3,10 @@ package com.whitelext.dotaHunter.view.screens
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import coil.annotation.ExperimentalCoilApi
 import com.whitelext.dotaHunter.ui.theme.Dota_hunterTheme
 import com.whitelext.dotaHunter.view.Home
@@ -17,6 +19,8 @@ class MainActivity : ComponentActivity() {
     @ExperimentalComposeUiApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
 
         setContent {
             Dota_hunterTheme {
